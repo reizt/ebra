63069a05be21c23411bd492caffa34109d54d87a
reizt <reizt.dev@gmail.com>
2022-07-20 15:27:44 +0900

    use conf.Migrate() instead of writing in handlers/users/main_test.go

--- handlers/users/main_test.go
+++ handlers/users/main_test.go
@@ -4,11 +4,9 @@ import (
 	"testing"
 
 	"github.com/reizt/ebra/conf"
-	"github.com/reizt/ebra/models"
 )
 
 func TestMain(m *testing.M) {
-	db := conf.ConnectMySQL()
-	db.AutoMigrate(&models.User{})
+	conf.Migrate()
 	m.Run()
 }
